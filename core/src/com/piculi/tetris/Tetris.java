a27afcb00365c7181b7c528189ab6e532143414a
Nikita Dergunov <nikita.dergunov@cantaa.com>
2024-05-03 14:30:18 +0200

    Initial Commit

---
+++ core/src/com/piculi/tetris/Tetris.java
@@ -0,0 +1,20 @@
+package com.piculi.tetris;
+
+import com.badlogic.gdx.Game;
+
+public class Tetris extends Game {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+}
---
+++ core/src/com/piculi/tetris/screens/MainMenuScreen.java
@@ -0,0 +1,4 @@
+package com.piculi.tetris.screens;
+
+public class MainMenuScreen {
+}
